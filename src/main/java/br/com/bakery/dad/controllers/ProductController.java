f676a2ea1cd30344c8d415c607446e5a726279e4
Pedro Esteves <kropinho211@outlook.com>
2023-11-23 18:21:55 -0300

    "status code correct in create, delete and update methods"

--- src/main/java/br/com/bakery/dad/controllers/ProductController.java
+++ src/main/java/br/com/bakery/dad/controllers/ProductController.java
@@ -3,9 +3,13 @@ package br.com.bakery.dad.controllers;
 import br.com.bakery.dad.dto.ProductDTO;
 import br.com.bakery.dad.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -31,13 +35,18 @@ public class ProductController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ProductDTO create(@RequestBody ProductDTO product){
-        return productService.create(product);
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO product, UriComponentsBuilder uriBuilder){
+        ProductDTO productDTO = productService.create(product);
+        UriComponents uriComponents = uriBuilder.path("/products/{id}").buildAndExpand(productDTO.getId());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+        return new ResponseEntity<>(productDTO, headers, HttpStatus.CREATED);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ProductDTO update(@RequestBody ProductDTO product){
-        return productService.update(product);
+    @PutMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO product){
+        ProductDTO updateProductDTO = productService.update(product);
+        return  ResponseEntity.ok(updateProductDTO);
     }
 
     @DeleteMapping(value = "/{id}")
--- src/main/java/br/com/bakery/dad/controllers/SaleController.java
+++ src/main/java/br/com/bakery/dad/controllers/SaleController.java
@@ -3,9 +3,13 @@ package br.com.bakery.dad.controllers;
 import br.com.bakery.dad.dto.SaleDTO;
 import br.com.bakery.dad.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -31,13 +35,18 @@ public class SaleController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SaleDTO create(@RequestBody SaleDTO sale){
-        return saleService.create(sale);
+    public ResponseEntity<SaleDTO> create(@RequestBody SaleDTO sale, UriComponentsBuilder uriBuilder){
+        SaleDTO saleDTO = saleService.create(sale);
+        UriComponents uriComponents = uriBuilder.path("/sales/{id}").buildAndExpand(saleDTO.getId());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+        return new ResponseEntity<>(saleDTO, headers, HttpStatus.CREATED);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public SaleDTO update(@RequestBody SaleDTO sale){
-        return saleService.update(sale);
+    @PutMapping(value = "/{id}" ,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SaleDTO> update(@PathVariable Long id, @RequestBody SaleDTO sale){
+        SaleDTO updateSaleDto = saleService.update(sale);
+        return ResponseEntity.ok(updateSaleDto);
     }
 
     @DeleteMapping(value = "/{id}")
